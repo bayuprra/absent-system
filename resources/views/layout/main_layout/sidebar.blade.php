e8a6bf3a04864e7269979793fe130dde8aa4e64c
Bayu Pratama <bayupratamaggg168@gmail.com>
2024-04-18 11:55:30 +0700

    fix automerge error

--- resources/views/layout/main_layout/sidebar.blade.php
+++ resources/views/layout/main_layout/sidebar.blade.php
@@ -4,16 +4,12 @@
 @endphp
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-
-
     <!-- Sidebar -->
     <div class="sidebar">
-                @if ($role == 'Admin')
-                        <!-- Sidebar user panel (optional) -->
+        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('AdminLTE/dist/img/avatar.png') }} " class="img-circle elevation-2"
-                    alt="User Image">
+                <img src="{{ asset('AdminLTE/dist/img/avatar.png') }} " class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{ $nama }}</a>
@@ -26,7 +22,7 @@
                 data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-
+                @if ($role == 'Admin')
                     <li class="nav-item">
                         <a href="{{ route('dashboard') }}" class="nav-link">
                             <i class=" nav-icon fas fa-tachometer-alt"></i>
@@ -61,24 +57,6 @@
                     </li>
                 @endif
                 @if ($role == 'karyawan')
-                        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ asset('AdminLTE/dist/img/avatar5.png') }} " class="img-circle elevation-2"
-                    alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">{{ session()->get('data')->nama_role ?? 'User' }}</a>
-            </div>
-        </div>
-
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-
                     <li class="nav-item">
                         <a href="{{ route('userAbsent') }}" class="nav-link">
                             <i class="nav-icon fas fa-user-alt"></i>
